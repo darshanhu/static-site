4520372667e6313272f13bad5fe544a25383d883
Darshan <darshan.hulswar@pendo.io>
2023-07-13 00:32:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let active = false;
+
+function toggle() {
+  let toggle = document.querySelector(".toggle");
+  let text = document.querySelector(".text");
+  active = !active;
+  if (active) {
+    toggle.classList.add("active");
+    text.innerHTML = "ON";
+  } else {
+    toggle.classList.remove("active");
+    text.innerHTML = "OFF";
+  }
+}
