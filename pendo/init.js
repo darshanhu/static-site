8c002604b8289262a7cd0d1b5275266f2836a586
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-09-01 23:43:29 +0530

    Update init.js

--- pendo/init.js
+++ pendo/init.js
@@ -34,3 +34,28 @@
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 })('43994d6b-66ea-49fc-71f1-31077e387150');
 
+pendo.initialize({
+        visitor: {
+            id:              'VISITOR-UNIQUE-ID'   // Required if user is logged in, default creates anonymous ID
+            // email:        // Recommended if using Pendo Feedback, or NPS Email
+            // full_name:    // Recommended if using Pendo Feedback
+            // role:         // Optional
+
+            // You can add any additional visitor level key-values here,
+            // as long as it's not one of the above reserved names.
+        },
+
+        account: {
+            id:           'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
+            // name:         // Optional
+            // is_paying:    // Recommended if using Pendo Feedback
+            // monthly_value:// Recommended if using Pendo Feedback
+            // planLevel:    // Optional
+            // planPrice:    // Optional
+            // creationDate: // Optional
+
+            // You can add any additional account level key-values here,
+            // as long as it's not one of the above reserved names.
+        }
+    });
+
