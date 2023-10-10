9e86c7527592683ba6984715e72a45b2b148684d
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-10-10 21:23:24 +0530

    Update init.js

--- pendo/init.js
+++ pendo/init.js
@@ -17,7 +17,8 @@
                 email:        "",
                 full_name:    "Darshan Hulswar",
                 typeTesting: 99,
-                Angus_PropertyID_new: [128, 129, 130, 131, 240]
+                Angus_PropertyID_new: [128, 129, 130, 131, 240],
+                birthDate: "1999-11-28"
             },
 
             account: {
