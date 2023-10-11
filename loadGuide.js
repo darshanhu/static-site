f6210dde14ec9f95e92471e0a895318cdad2319d
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-10-11 13:38:37 +0530

    update

--- loadGuide.js
+++ loadGuide.js
@@ -1,6 +1,17 @@
+const birthInfo = {
+    date: 11,
+    month: 10
+}
+
+const date = new Date();
+const today = date.getDate();
+const currentMonth = date.getMonth() + 1;
+
 window.addEventListener("load", () => {
     setTimeout(() => {
         console.log("From loadGuide.js")
-        pendo.showGuideById("ljF1w0DV486z1bp09uR0PWkpauI")
+        if( (today === birthInfo.date) && (currentMonth === birthInfo.month)) {
+            pendo.showGuideById("ljF1w0DV486z1bp09uR0PWkpauI")
+        }
     }, 3000);
 })
