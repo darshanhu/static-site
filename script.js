4332c3d39fee1c74036dfc14082be12aa70ee494
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-11-20 20:22:15 +0530

    update

--- script.js
+++ script.js
@@ -31,10 +31,16 @@ $('#exampleModal').on('shown.bs.modal', function () {
   // $('#myInput').trigger('focus')
 })
 
-if(window.innerWidth >= 1311 ) {
-  pendo.launchGuideById("tVYYILHD6bVvCa-4TZOf9sICX7g");
-  console.log("SCreen size changed")
-}
+
+
+window.addEventListener('resize', function(event) {
+
+  if(window.innerWidth >= 1311 ) {
+    pendo.launchGuideById("tVYYILHD6bVvCa-4TZOf9sICX7g");
+    console.log("SCreen size changed")
+  }
+  
+}, true);
 
 
 
