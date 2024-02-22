3915b2b255dead7fd0b9c1541aaf47b92828a989
darshanhu <133042615+darshanhu@users.noreply.github.com>
2024-02-22 18:14:03 +0530

    track event configured

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// let active = false;
+let active = false;
 
-// function toggle() {
-//   let toggle = document.querySelector(".toggle");
-//   let text = document.querySelector(".text");
-//   active = !active;
-//   if (active) {
-//     toggle.classList.add("active");
-//     text.innerHTML = "ON";
+function toggle() {
+  let toggle = document.querySelector(".toggle");
+  let text = document.querySelector(".text");
+  active = !active;
+  if (active) {
+    toggle.classList.add("active");
+    text.innerHTML = "ON";
 
-//     pendo.track("TOGGLE_STATUS", {
-//       toggleState: "ON"
-//     });
+    pendo.track("TOGGLE_STATUS", {
+      toggleState: "ON"
+    });
 
-//     console.log("Pendo Track Event Called with status 'ON'")
+    console.log("Pendo Track Event Called with status 'ON'")
     
-//   } else {
-//     toggle.classList.remove("active");
-//     text.innerHTML = "OFF";
+  } else {
+    toggle.classList.remove("active");
+    text.innerHTML = "OFF";
 
-//      pendo.track("TOGGLE_STATUS", {
-//       toggleState: "OFF"
-//     });
+     pendo.track("TOGGLE_STATUS", {
+      toggleState: "OFF"
+    });
 
-//     console.log("Pendo Track Event Called with status 'OFF'")
-//   }
-// }
+    console.log("Pendo Track Event Called with status 'OFF'")
+  }
+}
 
 
 $('#exampleModal').on('shown.bs.modal', function () {
