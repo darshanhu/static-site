ec9bd36227723f788a99d7c42016ca9a85945e1d
darshanhu <133042615+darshanhu@users.noreply.github.com>
2024-07-04 10:26:15 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const searchBox = document.querySelector("input[type='text']");
 
-let executed = false;
-searchBox.addEventListener("keyup", function(event) { 
-    if (event.key === "Enter") { 
-      if (!executed) {
-        executed = true;
-        // Do work i.e trigger the guide
-        pendo.showGuideById('ys7ZJPGskME_CNOaYSqG2_izzt4');
-      }   
-    } 
+function triggerGuideOnEnter() {
+  if (!executed) {
+    executed = true;
+    pendo.showGuideById('ys7ZJPGskME_CNOaYSqG2_izzt4');
+  }
+}
+
+searchBox.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    triggerGuideOnEnter();
+  }
 });
