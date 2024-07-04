6e3051e14a93046aff4e5af06996db2b47b08949
darshanhu <133042615+darshanhu@users.noreply.github.com>
2024-07-04 10:09:22 +0530

    update

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ searchBox.addEventListener("keyup", function(event) {
     if (event.key === "Enter") { 
         // Do work i.e trigger the guide
         pendo.showGuideById('ys7ZJPGskME_CNOaYSqG2_izzt4');
-        pendo.updateOptions( { visitor: {"id":"propertyUser-new"} }, { account: {"industry":"retail"} });
+        // pendo.updateOptions( { visitor: {"id":"propertyUser-new"} }, { account: {"industry":"retail"} });
+        pendo.stopGuides()
     } 
 });
 
