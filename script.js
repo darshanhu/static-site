e5a044239f1930441f3cd433e1725c240f017f17
darshanhu <darshan.hulswar@pendo.io>
2025-12-16 08:13:34 +0530

    mutation observer added for the guide question block

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
-const searchBox = document.querySelector("input[type='text']");
+// const searchBox = document.querySelector("input[type='text']");
 
-let executed = false;
-function triggerGuideOnEnter() {
-  if (!executed) {
-    executed = true;
-    pendo.showGuideById('ys7ZJPGskME_CNOaYSqG2_izzt4');
-  }
-}
+// let executed = false;
+// function triggerGuideOnEnter() {
+//   if (!executed) {
+//     executed = true;
+//     pendo.showGuideById('ys7ZJPGskME_CNOaYSqG2_izzt4');
+//   }
+// }
 
-searchBox.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    triggerGuideOnEnter();
+// searchBox.addEventListener("keyup", function (event) {
+//   if (event.key === "Enter") {
+//     triggerGuideOnEnter();
+//   }
+// });
+
+
+// (function () {
+//   // Delay execution for 10 seconds following the completion of page load
+//   const delayInMilliseconds = 10000;
+
+//   const executeAfterDelay = () => {
+//     // the below line checks for passed metadata: login_milestone using pendo.getSerializedMetadata() on visitor level and then checks for the condition if it matches then programatically trigger the guide using pendo.showGuideById("guideID")
+//     pendo.getSerializedMetadata().visitor.login_milestone
+//     let loginMieStone = pendo.getSerializedMetadata().visitor.login_milestone;
+
+//     if (loginMieStone === 10) {
+//       pendo.showGuideById("-4P7n-IFWKFMXrawJCJW88j1S5A")
+//     }
+//   };
+
+//   window.onload = () => {
+//     try {
+//       setTimeout(executeAfterDelay, delayInMilliseconds);
+//     } catch (error) {
+//       console.error('Error executing function after delay:', error);
+//     }
+//   };
+// })();
+
+const observer = new MutationObserver(() => {
+  const el = document.getElementById("pendo-text-99ea437f");
+  if (el) {
+    el.remove();
   }
 });
 
-
-(function () {
-  // Delay execution for 10 seconds following the completion of page load
-  const delayInMilliseconds = 10000;
-
-  const executeAfterDelay = () => {
-    // the below line checks for passed metadata: login_milestone using pendo.getSerializedMetadata() on visitor level and then checks for the condition if it matches then programatically trigger the guide using pendo.showGuideById("guideID")
-    pendo.getSerializedMetadata().visitor.login_milestone
-    let loginMieStone = pendo.getSerializedMetadata().visitor.login_milestone;
-
-    if (loginMieStone === 10) {
-      pendo.showGuideById("-4P7n-IFWKFMXrawJCJW88j1S5A")
-    }
-  };
-
-  window.onload = () => {
-    try {
-      setTimeout(executeAfterDelay, delayInMilliseconds);
-    } catch (error) {
-      console.error('Error executing function after delay:', error);
-    }
-  };
-})();
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
